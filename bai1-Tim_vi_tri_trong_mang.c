9d54097444c2844adfca687e08e1ef6406f82463
minhgialap <80458988+minhgialap@users.noreply.github.com>
2021-03-13 14:11:53 +0700

    Update bai1-Tim_vi_tri_trong_mang.c

--- bai1-Tim_vi_tri_trong_mang.c
+++ bai1-Tim_vi_tri_trong_mang.c
@@ -3,16 +3,22 @@
 int main(){
     int a[10];
     int b;
+    int KT=0;
     for(int i = 1; i<=10;i++){
         printf("\nnhap phan tu thu %d : ",i);scanf("%d",&a[i]);
        }
-    
+
     printf("\n nhap so: ");scanf("%d",&b);
        for(int i = 1; i<=10;i++){
            if(b == a[i]){
                   printf("vi tri cua so do la : %d ",i);
+                  KT=1;
                      break;
              }
           }
+
+      if(KT == 0){
+        printf("\nvi tri cua so do la: -1");
+      }
    return 0;
 }
